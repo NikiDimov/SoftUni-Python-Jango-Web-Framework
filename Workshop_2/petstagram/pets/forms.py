504f8729f1c8d2dec7b8d5d86f98d0aac60b0578
niki <niki.dimov86@gmail.com>
2021-08-04 19:43:37 +0300

    Petstagram_part_V

---
+++ Workshop_2/petstagram/pets/forms.py
@@ -0,0 +1,41 @@
+import os
+from os.path import join
+
+from django import forms
+from django.conf import settings
+
+from petstagram.core.forms import BootstrapFormMixin
+from petstagram.pets.models import Pet
+
+
+class PetForm(BootstrapFormMixin, forms.ModelForm):
+    class Meta:
+        model = Pet
+        exclude = ('user',)
+        widgets = {
+            'name': forms.TextInput(
+                attrs={
+                    'class': 'some-class',
+                }
+            ),
+        }
+
+
+class EditPetForm(PetForm):
+    def save(self, commit=True):
+        db_pet = Pet.objects.get(pk=self.instance.id)
+        if commit:
+            image_path = join(settings.MEDIA_ROOT, str(db_pet.image))
+            os.remove(image_path)
+        return super().save(commit)
+
+    class Meta:
+        model = Pet
+        fields = '__all__'
+        widgets = {
+            'type': forms.TextInput(
+                attrs={
+                    'readonly': 'readonly',
+                }
+            )
+        }
